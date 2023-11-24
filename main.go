27639b83be71156d0df8bc68e91d84dcff51f9bc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-11-24 16:54:36 +0530

    final commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func GetResponse(client gpt3.Client, ctx context.Context, question string) {
+	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			question,
+		},
+		MaxTokens:   gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	}, func(resp *gpt3.CompletionResponse) {
+		fmt.Print(resp.Choices[0].Text)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
+	fmt.Printf("\n")
+}
+
+type NullWriter int
+
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
+func main() {
+	log.SetOutput(new(NullWriter))
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	apiKey := viper.GetString("API_KEY")
+	if apiKey == "" {
+		panic("Missing API key")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+	rootCmd := &cobra.Command{
+		Use:   "chatgpt",
+		Short: "Chat with Chatgpt in Console",
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner := bufio.NewScanner(os.Stdin)
+			quit := false
+
+			for !quit {
+				fmt.Print("Say something ('quit' to end):")
+				if !scanner.Scan() {
+					break
+				}
+				question := scanner.Text()
+				switch question {
+				case "quit":
+					quit = true
+				default:
+					GetResponse(client, ctx, question)
+				}
+			}
+		},
+	}
+	rootCmd.Execute()
+}
